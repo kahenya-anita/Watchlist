ec9e76091047dcca982c71cc01083b6d415ca003
kahenya-anita <anitakahenya1@gmail.com>
2020-09-10 13:00:53 +0300

    Add WTF forms

--- app/request.py
+++ app/request.py
@@ -30,6 +30,26 @@ def get_movies(category):
 
     return movie_results
 
+def get_movie(id):
+    get_movie_details_url = base_url.format(id,api_key)
+
+    with urllib.request.urlopen(get_movie_details_url) as url:
+        movie_details_data = url.read()
+        movie_details_response = json.loads(movie_details_data)
+
+        movie_object = None
+        if movie_details_response:
+            id = movie_details_response.get('id')
+            title = movie_details_response.get('original_title')
+            overview = movie_details_response.get('overview')
+            poster = movie_details_response.get('poster_path')
+            vote_average = movie_details_response.get('vote_average')
+            vote_count = movie_details_response.get('vote_count')
+
+            movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
+
+    return movie_object
+
 def process_results(movie_list):
     '''
     Function  that processes the movie result and transform them to a list of Objects
@@ -55,22 +75,20 @@ def process_results(movie_list):
 
     return movie_results
 
-def get_movie(id):
-    get_movie_details_url = base_url.format(id,api_key)
+def search_movie(movie_name):
+    search_movie_url = 'https://api.themoviedb.org/3/search/movie?api_key=449c5f1e035a6c08d10cf904226a68be'.format(api_key,movie_name)
+    with urllib.request.urlopen(search_movie_url) as url:
+        search_movie_data = url.read()
+        search_movie_response = json.loads(search_movie_data)
 
-    with urllib.request.urlopen(get_movie_details_url) as url:
-        movie_details_data = url.read()
-        movie_details_response = json.loads(movie_details_data)
+        search_movie_results = None
 
-        movie_object = None
-        if movie_details_response:
-            id = movie_details_response.get('id')
-            title = movie_details_response.get('original_title')
-            overview = movie_details_response.get('overview')
-            poster = movie_details_response.get('poster_path')
-            vote_average = movie_details_response.get('vote_average')
-            vote_count = movie_details_response.get('vote_count')
+        if search_movie_response['results']:
+            search_movie_list = search_movie_response['results']
+            search_movie_results = process_results(search_movie_list)
 
-            movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
 
-    return movie_object
+    return search_movie_results
+
+
+
--- app/views.py
+++ app/views.py
@@ -1,9 +1,10 @@
-from flask import render_template
-from app import app
+from flask import render_template,request,redirect,url_for
+from flask import app
+from .request import get_movies,get_movie,search_movie
 
 # Views
-@app.route('/movie/<int:movie_id>')
-def movie(movie_id):
+@app.route('/movie/<int:id>')
+def movie(id):
 
     '''
     View movie page function that returns the movie page and its data
@@ -12,7 +13,7 @@ def movie(movie_id):
     title = f'{movie.title}'
     return render_template('movie.html',title = title,movie = movie)
 
-from .request import get_movies
+
 @app.route('/')
 def index():
 
@@ -22,6 +23,26 @@ def index():
 
     # Getting popular movie
     popular_movies = get_movies('popular')
-    print(popular_movies)
+    upcoming_movie = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
+
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title,popular = popular_movies)
+
+    search_movie = request.args.get('movie_query')
+
+    if search_movie:
+        return redirect(url_for('search',movie_name=search_movie))
+    else:
+        return render_template('index.html', title = title, popular = popular_movies, 
+        upcoming = upcoming_movie, now_showing = now_showing_movie )
+
+@app.route('/search/<movie_name>')
+def search(movie_name):
+    '''
+    View function to display the search results
+    '''
+    movie_name_list = movie_name.split(" ")
+    movie_name_format = "+".join(movie_name_list)
+    searched_movies = search_movie(movie_name_format)
+    title = f'search results for {movie_name}'
+    return render_template('search.html',movies = searched_movies)
